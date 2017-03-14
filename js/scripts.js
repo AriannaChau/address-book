2f32891053372fa3a0eb8512b11758a284b71f4e
Arianna Chau and Steven Galvin <steven@>
2017-03-13 20:44:23 -0700

    update html and js to include forms to gather address info and add it to the contact object, then return it back to the user.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 // business logic
-function Contact(first, last) {
+function Contact(first, last, street, city, state) {
   this.firstName = first;
   this.lastName = last;
+  this.street = street;
+  this.city = city;
+  this.state = state;
 }
+
+Contact.prototype.fullInfo = function() {
+  return this.firstName + " " + this.lastName + " " + this.street + " " + this.city + " " + this.state;
+}
+
  // user interface logic
  $(document).ready(function() {
    $("form#new-contact").submit(function(event) {
@@ -10,12 +18,18 @@ function Contact(first, last) {
 
      var inputtedFirstName = $("input#new-first-name").val();
      var inputtedLastName = $("input#new-last-name").val();
+     var inputtedStreet = $("input#new-street").val();
+     var inputtedCity = $("input#new-city").val();
+     var inputtedState = $("input#new-state").val();
 
-     var newContact = new Contact(inputtedFirstName, inputtedLastName);
+     var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedStreet, inputtedCity, inputtedState);
 
-     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullInfo() + "</span></li>");
 
      $("input#new-first-name").val("");
      $("input#new-last-name").val("");
+     $("input#new-street").val("");
+     $("input#new-city").val("");
+     $("input#new-state").val("");
    });
  });
